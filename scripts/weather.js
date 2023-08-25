5adc81e2135e68422e450e6f3b535df0420e541f
Samuel Victor Oliveira Lima <990905>
2023-08-25 14:56:27 -0300

    erro mesag

--- scripts/weather.js
+++ scripts/weather.js
@@ -6,18 +6,22 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-function getWeatherData(location) {
+async function getWeatherData(location) {
+    const weatherInfo = document.getElementById("weather-info");
     const apiKey = "0f329d77fcfb49899ce145714232508";
     const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`;
 
-    fetch(apiUrl)
+    await fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
             displayWeatherData(data);
         })
         .catch(error => {
-            console.error("Erro ao obter os dados do clima:", error);
-            alert('Lugar não encontrado!')
+            console.error("Erro", error);
+            weatherInfo.innerHTML = `
+            <h2>Place Not Found</h2>
+        `;
+        modalInfo.classList.remove("display-none");
         });
 }
 
